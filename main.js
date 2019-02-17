6c965dd809dfdeb87ab84ad1683be802a61211b9
Tony <antonio.ilersich@gmail.com>
2019-02-17 12:22:45 -0500

    added simple calculation and alert for when to wake up

--- main.js
+++ main.js
@@ -17,7 +17,20 @@ $(document).ready(function()
 {
 	console.log("page ready");
 	
-	//...
+	$("#imgoingtosleep").click(function()
+	{
+		var s = "Get up at one of these times:";
+		var time = Date.now();
+		time +=
+			14 * 60 * 1000 +	//14 minutes to fall asleep
+			90 * 60 * 1000;		//a 90-minute rem cycles
+		for (var i = 1; i < 5; i++)
+		{
+			s += "\n" + new Date(time) + ", ";
+			time += 90 * 60 * 1000; //add another 90-minute cycle
+		}
+		alert(s);
+	});
 	
 	refresh();
 });

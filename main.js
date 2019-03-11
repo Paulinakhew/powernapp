3633f15a37052d8f7d216b4bfb3f86252e6b2c2e
Tony <antonio.ilersich@gmail.com>
2019-03-10 21:52:09 -0400

    solve issue #7

--- main.js
+++ main.js
@@ -19,16 +19,37 @@ $(document).ready(function()
 	
 	$("#imgoingtosleep").click(function()
 	{
-		var s = "Get up at one of these times:";
-		var time = Date.now();
-		time +=
-			14 * 60 * 1000 +	//14 minutes to fall asleep
-			90 * 60 * 1000;		//a 90-minute rem cycles
-		for (var i = 1; i < 5; i++)
+		//consants to represent durations in ticks (milliseconds)
+		const TICKS_PER_SEC = 1000;
+		const TICKS_PER_MIN = 60 * TICKS_PER_SEC;
+		const TICKS_TO_FALL_ASLEEP = 14 * TICKS_PER_MIN;
+		const TICKS_PER_CYCLE = 90 * TICKS_PER_MIN;
+		
+		//the number of cycles to calculate
+		const N_CYCLES = 6;
+		
+		//begin writing user-readable message
+		var s = "Get up at one of these times:\n";
+		
+		//calculate time to set alarm
+		var alarmTime = Date.now() + TICKS_TO_FALL_ASLEEP;
+		
+		//add each cycle and record new time
+		for (var i = 0; i < N_CYCLES; i++)
 		{
-			s += "\n" + new Date(time) + ", ";
-			time += 90 * 60 * 1000; //add another 90-minute cycle
+			//calculate time
+			alarmTime += TICKS_PER_CYCLE;
+			var timeStr = (new Date(alarmTime)).toLocaleTimeString();
+			
+			//format separator
+			if (i != 0) s += ",   ";
+			
+			//record time
+			s += timeStr;
 		}
+		
+		//notify user
+		//TODO: ui instead of alert?
 		alert(s);
 	});
 	

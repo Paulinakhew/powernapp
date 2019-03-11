4f69e10b5a390ac5fa916fa1d39512c64cc0343e
Tony <antonio.ilersich@gmail.com>
2019-03-10 22:32:13 -0400

    solved issue #4, rewrote and documented some code

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+//constants to control the refresh rate
 const REFRESH_RATE = 1;
 const REFRESH_INTERVAL = 1 / REFRESH_RATE;
+
+//consants to represent durations in ticks (milliseconds)
+const TICKS_PER_SEC = 1000;
+const TICKS_PER_MIN = 60 * TICKS_PER_SEC;
+const TICKS_TO_FALL_ASLEEP = 14 * TICKS_PER_MIN;
+const TICKS_PER_REM_CYCLE = 90 * TICKS_PER_MIN;
+//TODO: is power nap 10 min or 15?
+const TICKS_PER_POWER_NAP = 10 * TICKS_PER_MIN;
+const TICKS_PER_NASA_NAP = 26 * TICKS_PER_MIN;
+const TICKS_PER_BAD_NAP = 30 * TICKS_PER_MIN;
+const TICKS_PER_SLOW_WAVES_NAP = 60 * TICKS_PER_MIN;
 
 function refresh()
 {
@@ -17,19 +29,13 @@ $(document).ready(function()
 {
 	console.log("page ready");
 	
-	$("#imgoingtosleep").click(function()
+	$("#deep-sleep-submit").click(function()
 	{
-		//consants to represent durations in ticks (milliseconds)
-		const TICKS_PER_SEC = 1000;
-		const TICKS_PER_MIN = 60 * TICKS_PER_SEC;
-		const TICKS_TO_FALL_ASLEEP = 14 * TICKS_PER_MIN;
-		const TICKS_PER_CYCLE = 90 * TICKS_PER_MIN;
-		
 		//the number of cycles to calculate
 		const N_CYCLES = 6;
 		
 		//begin writing user-readable message
-		var s = "Get up at one of these times:\n";
+		var msg = "Get up at one of these times:\n";
 		
 		//calculate time to set alarm
 		var alarmTime = Date.now() + TICKS_TO_FALL_ASLEEP;
@@ -38,19 +44,50 @@ $(document).ready(function()
 		for (var i = 0; i < N_CYCLES; i++)
 		{
 			//calculate time
-			alarmTime += TICKS_PER_CYCLE;
+			alarmTime += TICKS_PER_REM_CYCLE;
 			var timeStr = (new Date(alarmTime)).toLocaleTimeString();
 			
 			//format separator
-			if (i != 0) s += ",   ";
+			if (i != 0) msg += ",   ";
 			
 			//record time
-			s += timeStr;
+			msg += timeStr;
 		}
 		
 		//notify user
 		//TODO: ui instead of alert?
-		alert(s);
+		alert(msg);
+	});
+	
+	$("#quick-nap-submit").click(function()
+	{
+		console.log("click");
+		
+		//find selected nap
+		var options = [
+			"ThePowerNap",
+			"TheNASANap",
+			"TheBadNap",
+			"TheSlow-WaveSleepNap",
+			"TheFullSleepCycleNap"];
+		var times = [
+			TICKS_PER_POWER_NAP,
+			TICKS_PER_NASA_NAP,
+			TICKS_PER_BAD_NAP,
+			TICKS_PER_SLOW_WAVES_NAP,
+			TICKS_PER_REM_CYCLE];
+		var selection = $("#nap-select").val();
+		var index = options.indexOf(selection);
+		var napTicks = times[index];
+		
+		//calculate time to set alarm
+		var alarmTime = Date.now() + TICKS_TO_FALL_ASLEEP + napTicks;
+		var timeStr = (new Date(alarmTime)).toLocaleTimeString();
+		var msg = "Get up at " + timeStr;
+		
+		//notify user
+		//TODO: ui instead of alert?
+		alert(msg);
 	});
 	
 	refresh();
